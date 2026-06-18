418bb6364397e60dbb4979f22da5dfd06453dee1
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-06-18 15:56:15 +0530

    Add files via upload

--- src/pages/Rest.jsx
+++ src/pages/Rest.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { version } from "react";
 
 const Rest = () => {
-  return (
-    <div>Rest</div>
-  )
-}
+  // const a = [10,20,30];
+  // const b = [100,200,300];
+  // const c = [...a, ...b];
+  // console.log(c);
 
-export default Rest
+  // const obj = {
+  //   name: "Node",
+  //   version: 24,
+  //   relese: 2009
+  // }
+
+  // const update = {
+  //   ...obj,
+  //   version: 25
+  // }
+  // const newAdd = {
+  //   ...obj,
+  //   relese: 2009
+  // }
+
+  // const {version, ...details} = obj;
+  // console.log(details);
+
+  // function sum(...num){
+  //   return num.reduce((a, b)=>a + b, 0);
+  // }
+  // const res = sum(10,20,30,40,50)
+  // console.log(res);
+
+  const tech = [
+    { id: 1, name: "Node" },
+    { id: 2, name: "React" },
+  ];
+  // const newAdd = [
+  //   ...tech,
+  //   {id:3, name:"js"}
+  // ]
+  const update = tech.map((tech) => {
+    if (tech.id === 1) {
+      return { ...tech, name: "Java" };
+    }
+    return tech;
+  });
+  console.log(update);
+
+  return <div>Rest</div>;
+};
+
+export default Rest;
